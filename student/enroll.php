816168058db522da8c48500c2b80046b6e3feef5
Rae™ <raebloodmage@gmail.com>
2015-04-28 21:35:48 +0800

    adjust

--- student/enroll.php
+++ student/enroll.php
@@ -1,13 +1,18 @@
 <html>
     <link href="css/style1.css" rel="stylesheet" type="text/css"/>
     
-    <head> <title>"Enroll to University"</title></head>
+    <head> 
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta charset="UTF-8" name="viewport" content="width=device-width,initial-scale=1"/>
+        <link href="css/style1.css" type="text/css" rel="stylesheet" title="default" />
+        <title>"Enroll to University"</title>
+    </head>
     
     <body>
         <?php include 'header.html'?>
         <?php include 'menu.html'?>
         
-        <div id="maincontent">
+        <div id="divcontent">
         
             <div id="content">
                 <a href="block.php"><img class="image"src="img/schedule.png">Block</a>
