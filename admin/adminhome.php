5ba887f1da0043d0be2b8466bdc65df436ea978b
tinemary <tinmagsombol@gmail.com>
2015-04-29 14:31:56 +0800

    Changed sizes of icons in adminhome.php

--- admin/adminhome.php
+++ admin/adminhome.php
@@ -19,19 +19,15 @@
 		<div class="menumain">
 		
 			<li id="enroll"><a href="adminenrollment.html">
-				<img src="img/enroll.png" alt="Enroll" style="width:300px;height:300px;">Approve Enrollment</a></li>
+				<img src="img/enroll.png" alt="Enroll" style="width:200px;height:200px;">Approve Enrollment</a></li>
 			
 			<li id="course"><a href="admincourse.html">
-				<img src="img/course.png" alt="Course" style="width:300px;height:300px;">Manage Courses</a></li>
-						
-			<li id="subj"><a href="adminsubject.html">
-				<img src="img/subjects.png" alt="Subjects" style="width:300px;height:300px;">Manage Subjects</a></li>
-				
+				<img src="img/course.png" alt="Course" style="width:200px;height:200px;">Manage Subjects</a></li>
 			<li id="sched"><a href="adminschedule.html">
-				<img src="img/schedule.jpg" alt="Schedule" style="width:300px;height:300px;">Manage Schedule</a></li>
+				<img src="img/schedule.jpg" alt="Schedule" style="width:200px;height:200px;">Manage Schedule</a></li>
 				
 		 <li id="studList"><a href="adminstudents.html">
-				<img src="img/students.jpg" alt="StudentsList" style="width:300px;height:300px;">Students List</a></li>
+				<img src="img/students.jpg" alt="StudentsList" style="width:200px;height:200px;">Students List</a></li>
 		</div>
             <div id="ex"></div>
                 <?php include 'footer.html'?>
