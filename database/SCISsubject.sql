65cf85823be011248b737c6173f0b371fd4bf296
LaigoRamel <laigoramel@gmail.com>
2015-05-01 14:50:04 +0800

    finished SCIS subjects

--- database/SCISsubject.sql
+++ database/SCISsubject.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2015 at 07:26 AM
+-- Generation Time: May 01, 2015 at 08:45 AM
 -- Server version: 5.6.24-log
 -- PHP Version: 5.5.12
 
@@ -17,10 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `coursessubject`
+-- Database: `scissubject`
 --
-CREATE DATABASE `coursessubject` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `coursessubject`;
+CREATE DATABASE `scissubject` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `scissubject`;
 -- --------------------------------------------------------
 
 --
@@ -148,13 +148,149 @@ CREATE TABLE IF NOT EXISTS `2ndyearscis2ndsem` (
 INSERT INTO `2ndyearscis2ndsem` (`courseNo`, `descriptiveTitle`, `totalUnits`) VALUES
 ('ECON 1', 'BASIC ECONOMICS WITH TAXATION AND AGRARIAN REFORM', 3),
 ('ENGL 7', 'TECHNICAL WRITING', 3),
-('IT 122', 'PROGRAMMING FUNDAMENTALS II', 2),
-('IT 122L', 'PROGRAMMING FUNDAMENTALS II', 1),
+('IT 222', 'OPERATING SYSTEMS', 2),
+('IT 222L', 'OPERATING SYSTEMS', 1),
 ('P E 4', 'TEAM SPORTS/GAMES', 2),
 ('PHYS 1A', 'COLLEGE PHYSICS 1', 3),
 ('PHYS 1AL', 'COLLEGE PHYSICS 1', 1),
 ('PSYCHO 1', 'GENERAL PSYCHOLOGY WITH DRUG PREVENTION', 3),
 ('THEO 4', 'LIVING LIKE JESUS', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `2ndyearscissummer`
+--
+
+CREATE TABLE IF NOT EXISTS `2ndyearscissummer` (
+  `courseNo` varchar(20) NOT NULL,
+  `descriptiveTitle` varchar(60) NOT NULL,
+  `totalUnits` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `2ndyearscissummer`
+--
+
+INSERT INTO `2ndyearscissummer` (`courseNo`, `descriptiveTitle`, `totalUnits`) VALUES
+('IT 221', 'DISCRETE MATHEMATICS', 3),
+('NSTP-CWTS 2', 'SOCIAL AWARENESS AND EMPOWERMENT FOR SERVICE', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `3rdyearscis1stsem`
+--
+
+CREATE TABLE IF NOT EXISTS `3rdyearscis1stsem` (
+  `courseNo` varchar(20) NOT NULL,
+  `descriptiveTitle` varchar(60) NOT NULL,
+  `totalUnits` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `3rdyearscis1stsem`
+--
+
+INSERT INTO `3rdyearscis1stsem` (`courseNo`, `descriptiveTitle`, `totalUnits`) VALUES
+('IT 312', 'PROGRAMMING APPLICATIONS', '2'),
+('IT 312L', 'PROGRAMMING APPLICATIONS', '1'),
+('IT 315', 'DATABASE MANAGEMENT SYSTEMS', '2'),
+('IT 315L', 'DATABASE MANAGEMENT SYSTEMS', '1'),
+('IT 317', 'FIINANCIAL SYSTEMS', '3'),
+('IT 321', 'INFORMATION MANAGEMENT CONCEPTS AND FUNDAMENTALS', '3'),
+('ITF 002', 'PERSONALITY DEVELOPMENT', '3'),
+('IT 311', 'SOFTWARE MODELING AND ANALYSIS', '3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `3rdyearscis2ndsem`
+--
+
+CREATE TABLE IF NOT EXISTS `3rdyearscis2ndsem` (
+  `courseNo` varchar(20) NOT NULL,
+  `descriptiveTitle` varchar(60) NOT NULL,
+  `totalUnits` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `3rdyearscis2ndsem`
+--
+
+INSERT INTO `3rdyearscis2ndsem` (`courseNo`, `descriptiveTitle`, `totalUnits`) VALUES
+('IT 322', 'NETWORKS TECHNOLOGY WITH ADMINISTRATION AND MAINTENANCE', 2),
+('IT 322L', 'NETWORKS TECHNOLOGY WITH ADMINISTRATION AND MAINTENANCE', 1),
+('IT 323', 'INTEGRATIVE PROGRAMMING AND TECHNOLOOGIES', 2),
+('IT 323L', 'INTEGRATIVE PROGRAMMING AND TECHNOLOOGIES', 1),
+('IT 324', 'WEB SYSTEMS AND TECHNOLOGIES', 2),
+('IT 324L', 'WEB SYSTEMS AND TECHNOLOGIES', 1),
+('IT 412A', 'PRACTICUM 1', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `3rdyearscissummer`
+--
+
+CREATE TABLE IF NOT EXISTS `3rdyearscissummer` (
+  `courseNo` varchar(20) NOT NULL,
+  `descriptiveTitle` varchar(60) NOT NULL,
+  `totalUnits` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `3rdyearscissummer`
+--
+
+INSERT INTO `3rdyearscissummer` (`courseNo`, `descriptiveTitle`, `totalUnits`) VALUES
+('IT 412B', 'PRACTICUM 2', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `4thyearscis1stsem`
+--
+
+CREATE TABLE IF NOT EXISTS `4thyearscis1stsem` (
+  `courseNo` varchar(20) NOT NULL,
+  `descriptiveTitle` varchar(60) NOT NULL,
+  `totalUnits` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `4thyearscis1stsem`
+--
+
+INSERT INTO `4thyearscis1stsem` (`courseNo`, `descriptiveTitle`, `totalUnits`) VALUES
+('IT 411', 'IT PROJECT 1', 3),
+('IT 422', 'SOCIAL AND PROFFESIONAL ISSUES IN INFORMATION TECHNOLOGY', 3),
+('IT 424', 'FIELD TRIPS AND SEMINARS', 3),
+('MATH 7AN', 'FUNDAMENTALS OF PROBABILITY AND STATISTICS', 3),
+('IT 325N', 'SOFTWARE ENGINEERING FUNDAMENTALS', 3),
+('IT 314', 'SYSTEM INTEGRATION AND ARCHITECTURE', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `4thyearscis2ndsem`
+--
+
+CREATE TABLE IF NOT EXISTS `4thyearscis2ndsem` (
+  `courseNo` varchar(20) NOT NULL,
+  `descriptiveTitle` varchar(90) NOT NULL,
+  `totalUnits` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `4thyearscis2ndsem`
+--
+
+INSERT INTO `4thyearscis2ndsem` (`courseNo`, `descriptiveTitle`, `totalUnits`) VALUES
+('IT 421', 'IT PROJECT 2', 3),
+('IT 423', 'INFORMATION SYSTEMS PLANNING / INFORMATION RESOURCE PLANNING', 3),
+('IT 413', 'INFORMATION ASSURANCE AND SECURITY', 3),
+('POLIT SC 1N', 'POLITICS & GOVERNANCE W/ PHILIPPINE CONSTITUTION', 3);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
