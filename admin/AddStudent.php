af04dc1f4360b12fc2c06bbd5659b0c566263225
florachu <totally_chin2121@hotmail.com>
2015-05-16 14:30:20 +0800

    Update AddStudent.php

--- admin/AddStudent.php
+++ admin/AddStudent.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<!--Chin, Maria___changed css style and added new tags, form 'addStudent'-->
+
 <html>
 	<head>
 		<meta charset="UTF-8" name="viewport" content="width=device-width,initial-scale=1"/>
@@ -13,17 +15,17 @@
         <?php include 'menu.html'?>
 
 		<form id = "addStudent">
-			<label id="studentIDno">ID Number:</label>
-			<input type="text" name="idno"><br>
-	                    <label id="studentLname">Last Name:</label>
-				<input type="text" name="lname"><br>
-	                    <label id="studentFname">First Name:</label>
-				<input type="text" name="fname"><br>
-	                    <label id="studentCourse">Course:</label>
-				<input type="text" name="course"><br>
-	                    <label id="studentYear">Year:</label>
-				<input type="text" name="yr"><br>
-				<input type="submit" value="Save">
+                    <label id="studentIDno">ID Number:</label>
+			<input type="text" name="idno" id="inputId"><br>
+                    <label id="studentLname">Last Name:</label>
+			<input type="text" name="lname" id="inputLname"><br>
+                    <label id="studentFname">First Name:</label>
+			<input type="text" name="fname" id="inputFname"><br>
+                    <label id="studentCourse">Course:</label>
+			<input type="text" name="course" id="inputCourse"><br>
+                    <label id="studentYear">Year:</label>
+			<input type="text" name="yr" id="inputYear"><br>
+			<input type="submit" value="Save">
 		</form>
 
 		<?php include 'footer.html'?>
